477334c54841d1a6800bdb16b0ab2db6c9a0dce2
vishsalunke14 <142652220+vishsalunke14@users.noreply.github.com>
2025-02-03 17:06:56 +0530

    Update Home.jsx

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <h2>Home Components</h2>
+        <>
+            <h2 className='text-center'>Home Components</h2>
+            <div className="d-flex justify-content-center  align-items-center bg-secondary vw-50 vh-50">
+                <img src=''></img>
+                <Link to={'/register'} className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>
+                    Register
+                </Link>
+                <Link to={'/login'} className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>
+                    Login
+                </Link>
+            </div>
+        </>
     )
 }
 
