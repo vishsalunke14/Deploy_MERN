7cc21184d2d3f1f7336bdece7852718aadbf5ac0
vishsalunke14 <142652220+vishsalunke14@users.noreply.github.com>
2025-02-03 16:47:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
      {
-        origin: [""],
+        origin: ["https://deploy-mern-frontend-mauve.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
